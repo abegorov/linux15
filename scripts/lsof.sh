bac4f2e29c195ca1e976ae07e7e29b46b4efa157
abegorov <abegorov@github.com>
2024-08-21 22:24:41 +0300

    Доработка скрипта lsof.sh

--- scripts/lsof.sh
+++ scripts/lsof.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-set -o nounset  # exit immediately on expanding a variable that is not set
+set -o nounset   # exit immediately on expanding a variable that is not set
+set -o pipefail  # the return value of a pipeline is the status of the last
+                 # command to exit with a non-zero status, or zero if no
+                 # command exited with a non-zero status
 
-echo 'COMMAND              PID   UID  FD NAME'
+# Печатает на экран строку в нужном формате с информацией о файле,
+# которая берётся из переменных среды
+function line()
+{
+  printf '%-27s %7s %5s %3s%-1s %s\n' \
+    "${COMMAND}" "${PID}" "${USERID}" "${FD}" "${ACCESS}" "${NAME}"
+}
+
+COMMAND='COMMAND'
+PID='PID'
+USERID='UID'
+FD='FD'
+ACCESS=''
+NAME='NAME'
+line
 
 find /proc/ -mindepth 1 -maxdepth 1 -type d -regex '/proc/[0-9]+' \
 | while read dir; do
@@ -10,12 +27,44 @@ find /proc/ -mindepth 1 -maxdepth 1 -type d -regex '/proc/[0-9]+' \
     || continue
   USERID="$(cat "${dir}/status" | grep 'Uid:' | cut -d $'\t' -f 2 )" \
     || continue
+
+  ACCESS=''
+
+  FD='cwd'
+  NAME=$(readlink "${dir}/cwd") || continue
+  line
+
+  FD='rtd'
+  NAME=$(readlink "${dir}/root") || continue
+  line
+
+  FD='txt'
+  NAME=$(readlink "${dir}/exe") || continue
+  line
+
+  FD='mem'
+  cat "${dir}/maps" | grep --only-matching '/.*' | uniq \
+  | while read file; do
+    NAME="${file}"
+    line
+  done
+
   find "${dir}/fd/" -mindepth 1 -maxdepth 1 -type l \
   | while read file; do
+
+    STAT=$(stat --format='%A' "${file}") || continue
+    if [[ ${STAT} =~ ^lrw.+ ]]; then
+      ACCESS='u'
+    elif [[ ${STAT} =~ ^lr.+ ]]; then
+      ACCESS='r'
+    elif [[ ${STAT} =~ ^l-w.+ ]]; then
+      ACCESS='w'
+    else
+      ACCESS=''
+    fi
+
     FD=$(basename "${file}") || continue
     NAME=$(readlink "${file}") || continue
-
-    printf '%-16s %7s %5s %3s %s\n' \
-      "${COMMAND}" "${PID}" "${USERID}" "${FD}" "${NAME}"
+    line
   done
 done
